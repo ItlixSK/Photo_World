62013c6740fdf6456bf486f18271953dfcabb15a
Sergey <109138599+ItlixSK@users.noreply.github.com>
2022-07-26 21:26:59 +0300

    Stage-2 Navigation Component

--- app/src/main/java/com/example/photoworld/ui/GalleryFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.example.photoworld.ui
-
-import androidx.fragment.app.Fragment
-
-class GalleryFragment : Fragment() {
-
-}
---
+++ app/src/main/java/com/example/photoworld/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,8 @@
+package com.example.photoworld.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.example.photoworld.R
+
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+
+}
