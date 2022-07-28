8ea9c447dc7bef8491b0a8fa917af22f3de3b52e
Sergey <109138599+ItlixSK@users.noreply.github.com>
2022-07-28 17:45:53 +0300

    Stage-8 SearchView Menu

--- app/src/main/java/com/example/photoworld/Constants.kt
+++ app/src/main/java/com/example/photoworld/Constants.kt
@@ -3,4 +3,4 @@ package com.example.photoworld
 const val CLIENT_ID = BuildConfig.UNSPLASH_ACCESS_KEY
 const val BASE_URL = "https://api.unsplash.com/"
 const val STARTING_PAGE_INDEX = 1
-const val DEFAULT_QUERY = "cats"
+const val DEFAULT_QUERY = "auto"
--- app/src/main/java/com/example/photoworld/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/photoworld/ui/gallery/GalleryFragment.kt
@@ -1,7 +1,10 @@
 package com.example.photoworld.ui.gallery
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.photoworld.R
@@ -25,14 +28,41 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
         binding.apply {
             recyclerView.setHasFixedSize(true)
             recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
-                header = PhotoLoadStateAdapter{adapter.retry()},
-                footer = PhotoLoadStateAdapter{adapter.retry()}
+                header = PhotoLoadStateAdapter { adapter.retry() },
+                footer = PhotoLoadStateAdapter { adapter.retry() }
             )
         }
 
         viewModel.photo.observe(viewLifecycleOwner) {
-         adapter.submitData(viewLifecycleOwner.lifecycle, it)
+            adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.search_menu, menu)
+        val searchItem = menu.findItem(R.id.actionSearch)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                if (query != null) {
+                    binding.recyclerView.scrollToPosition(0)
+                    viewModel.searchPhoto(query)
+                    searchView.clearFocus()
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+
+        })
+
     }
 
     override fun onDestroy() {
